8294b2132b44ea3d707382ae1070c89df068da32
Jimmy Vela <jimmyvela06@.com>
2022-12-07 03:37:05 -0600

    db bugs/fixes

--- db/schema.sql
+++ db/schema.sql
@@ -5,26 +5,27 @@ USE employeeTracker_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(60),
-    
+    department_name VARCHAR(30) NOT NULL    
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(60),
-    salary DECIMAL(10,2),
     department_id INT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
     FOREIGN KEY(department_id)
     REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(60) NOT NULL,
-    last_name VARCHAR(60) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
     ON DELETE SET NULL
 );
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,19 @@
-INSERT INTO departments (department_name)
+INSERT INTO department (department_name)
 VALUES ("Executive"),
        ("Sales"),
        ("Engineering"),
        ('IT');
 
 INSERT INTO roles (department_id, title, salary) 
-VALUES ("CEO", 800000, 1),
-       ("Sales Mangager", 100000, 2),
-       ("Sales Clerk", 60000, 2),
-       ("Engineer Lead", 160000, 3),
-       ("Senior Developer", 115000, 3),
-       ("Junior Developer", 75000, 3),
-       ("System Admin", 80000, 4);
+VALUES (1, "CEO", 800000),
+       (2, "Sales Mangager", 100000),
+       (2, "Sales Clerk", 60000),
+       (3, "Engineer Lead", 160000),
+       (3, "Senior Developer", 115000),
+       (3, "Junior Developer", 75000),
+       (4,"System Admin", 80000);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES ("Lucas", "Brown", 1, NULL),
        ("Alfred", "Tucker", 2, NULL),
        ("Miller", "Montgomery", 3, 2),
