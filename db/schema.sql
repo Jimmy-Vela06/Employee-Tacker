000f6693fea6d04efa668afa966bf1c5f6d36a91
Jimmy Vela <jimmyvela06@.com>
2022-12-07 02:42:35 -0600

    seeds add

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR(60),
     salary DECIMAL(10,2),
     department_id INT,
     FOREIGN KEY(department_id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,29 @@
-DROP DATABASE IF EXISTS employeeTracker_db;
-CREATE DATABASE employeeTracker_db;
+INSERT INTO departments (department_name)
+VALUES ("Executive"),
+       ("Sales"),
+       ("Engineering"),
+       ('IT');
 
-USE employeeTracker_db;
+INSERT INTO roles (department_id, title, salary) 
+VALUES ("CEO", 800000, 1),
+       ("Sales Mangager", 100000, 2),
+       ("Sales Clerk", 60000, 2),
+       ("Engineer Lead", 160000, 3),
+       ("Senior Developer", 115000, 3),
+       ("Junior Developer", 75000, 3),
+       ("System Admin", 80000, 4);
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(60),
-    
-);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES ("Lucas", "Brown", 1, NULL),
+       ("Alfred", "Tucker", 2, NULL),
+       ("Miller", "Montgomery", 3, 2),
+       ("Heather", "Rogers", 3, 2),
+       ("David", "Howard", 4, NULL),
+       ("Darcy", "Cole", 5, 5),
+       ("Adele", "Smith", 6, 5),
+       ("George", "Wilson", 6, 5),
+       ("Alexander", "Henderson", 7, NULL);
 
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT,
-    FOREIGN KEY(department_id)
-    REFERENCES department(id)
-);
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(60) NOT NULL,
-    last_name VARCHAR(60) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
-    ON DELETE SET NULL
-);
+
 
