da04bff06035813723c575b5236658507681965a
Jimmy Vela <jimmyvela06@.com>
2022-12-12 03:26:04 -0600

    view/add deparment,roles,employees

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const consoleTables = require("console.table");
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
-// Connect to database
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -22,7 +20,6 @@ db.connect(function (err) {
 const employee_db = () => {
   console.log("\n");
   console.log("\x1b[32m%s\x1b[0m", "EMPLOYEE DATABASE");
-  console.log("\n");
   return inquirer
     .prompt([
       {
@@ -35,7 +32,7 @@ const employee_db = () => {
           "View Employees",
           "Add Department",
           "Add Role",
-          "Add Emplopee",
+          "Add Employee",
           "Update Employee Role",
           "Exit Database!",
         ],
@@ -58,20 +55,144 @@ const employee_db = () => {
         update_role();
       } else {
         console.log("Have a nice day!");
-        process.quiet();
+        db.end();
       }
     });
 };
 
 const view_departments = () => {
-  let query = "SELECT * FROM department";
+  let department_db = "SELECT * FROM department";
 
-  db.query(query, function (err, res) {
+  db.query(department_db, function (err, res) {
     if (err) throw err;
     console.table(res);
-    console.log("\n");
+    employee_db();
   });
 };
+
+const view_roles = () => {
+  //   let roles_db = "SELECT * FROM roles";
+
+  //   db.query(roles_db, function (err, res) {
+  //     if (err) throw err;
+  //     console.table(res);
+  //     employee_db();
+  //   });
+  let roles_db = `SELECT
+        d.department_name,
+        r.title,
+        r.id,
+        r.salary 
+      FROM roles r
+      JOIN department d
+      ON r.department_id=d.id;`;
+  db.query(roles_db, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    employee_db();
+  });
+};
+
+const view_employees = () => {
+  let employees_db = `SELECT 
+           e.id,
+           e.first_name,
+           e.last_name,
+           r.title,
+           d.department_name,
+           r.salary,
+           CONCAT (e2.first_name, " ",e2.last_name) AS manager 
+        FROM employee e 
+        JOIN roles r 
+        ON e.role_id = r.id
+        LEFT JOIN employee e2
+        ON e.manager_id = e2.id
+        JOIN department d
+        ON r.department_id=d.id 
+      ;`;
+
+  db.query(employees_db, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    employee_db();
+  });
+};
+
+const add_department = () => {
+  inquirer
+    .prompt({
+      name: "department_name",
+      type: "input",
+      message: "What ist the name of the department you wish to add?",
+    })
+    .then((department) => {
+      db.query("INSERT INTO department SET ?", department);
+      console.log("\n");
+      console.log("Department added!");
+      view_departments();
+    });
+};
+
+const add_role = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What role would you like to add?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary for this role?",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "What is the department id?",
+      },
+    ])
+    .then((role) => {
+      db.query("INSERT INTO roles SET ?", role);
+      console.log("\n");
+      console.log("Role added!");
+      view_roles();
+    });
+};
+
+const add_employee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "Input new employee's first name?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Input new employee's last name?",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "What is the role id of the new employee?",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "What is the manager id of the new employee?",
+      },
+    ])
+    .then((employee) => {
+      db.query("INSERT INTO employee SET ?", employee);
+      console.log("\n");
+      console.log("Employee added!");
+      view_employees();
+    });
+};
+
+employee_db();
 
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}🚀`);
