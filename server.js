38c3a864c6d2dce3112b19b1b33defc73f1ca917
Jimmy Vela <jimmyvela06@.com>
2022-12-20 01:44:41 -0600

    finale fixes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
+const cTable = require("console.table");
+
 // const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection(
